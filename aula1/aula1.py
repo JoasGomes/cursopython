64a7adad8fa9caf7bf64adbb6751a58822885bae
JoasGomes <jojoca.vitor777@gmail.com>
2022-09-04 19:52:19 -0300

    Comentários e Print

---
+++ aula1/aula1.py
@@ -0,0 +1,28 @@
+print('hello world!')
+print('linha 1')
+print('linha 2')
+# falando do que se trata
+print('linha 3')
+print('linha 4')  # isso é um comentário da linha 4
+print('linha 5')
+# isso é um comentário
+
+"""
+comentário 
+de 
+várias linhas
+"""
+# função print - função que exibe argumento escolhido
+print(12345)
+
+# não vai ter nenhuma quebra de linha no final
+print('joas', 'vitor', end='####')
+print('joas', 'vitor', sep='-')  # indicando que o separador seja '-'
+
+print('333', end='.')
+print('333', end='.')
+print('333', end='-')
+print('33')
+
+print('333', '333', '333', sep='.', end='-')
+print('33')
