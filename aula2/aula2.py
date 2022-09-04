3fafb43405412b2404e4d93c0ef68d9b808d5727
JoasGomes <jojoca.vitor777@gmail.com>
2022-09-04 20:23:32 -0300

    Strings e Tipos Primitivos

---
+++ aula2/aula2.py
@@ -0,0 +1,30 @@
+# str - string
+# python é de tipagem dinâmica, ele infere o tipo
+print('alguma string')
+print("aspas dupla também")
+print(1234566788)
+# aspas dentro das aspas
+print("essa é uma 'string' (str)")
+
+# caracter de escape
+print('esse é meu \'texto\' (str).')
+print("esse é meu \"texto\" (str).")
+
+# quebra de linha
+print('essa é minha quebra \nquebrou')
+
+
+# tipos de dados
+# string - 'texto'
+# boolean - true or false
+# int - numeros inteiros = 1, 22
+# float - numeros de ponto flutuante = 1.2,3.5
+
+print('joas', type('joas'))  # vai mostrar o tipo de dado
+print(1, type(1))
+print(10 == 10, type(10 == 10))
+print(1.5, type(1.5))
+
+print(bool(0))
+print(bool(''))
+print(bool([]))
